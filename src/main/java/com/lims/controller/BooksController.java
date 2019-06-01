bd72554fdf9820db2cbdccb849be01d69aafff30
Dell <Dell@DESKTOP-51B718R>
2019-06-01 17:21:28 +0700

    hihi

--- src/main/java/com/lims/controller/BooksController.java
+++ src/main/java/com/lims/controller/BooksController.java
@@ -1,5 +1,10 @@
 package com.lims.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -8,7 +13,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lims.entity.Book;
 import com.lims.service.BookService;
@@ -24,7 +28,16 @@ public class BooksController {
 			) {
 		model.addAttribute("books", bookService.getBookdAll());
 		Page<Book> pageBook = bookService.getBookAll(new PageRequest(page, 8));
-		model.addAttribute("bookPage", pageBook);
+		model.addAttribute("bookPage", pageBook.getContent());
+		int totalPage = pageBook.getTotalPages();
+		if(totalPage>0) {
+			List<Integer> pageNumbers = new ArrayList<>();
+			for (int i = 0; i < totalPage; i++) {
+				pageNumbers.add(i+1);
+			}
+			model.addAttribute("pageNumbers",pageNumbers);
+			
+		}
 		return "views/books/book";
 	}
 //	@RequestMapping(value = "/book/list-book/page/{page}", method = RequestMethod.GET)
