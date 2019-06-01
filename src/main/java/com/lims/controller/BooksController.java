bc783a5ade3b08246abc8cb6ca65ca3033222a8a
sinhviencoder <48917649+sinhviencoder@users.noreply.github.com>
2019-06-01 17:19:20 +0700

    Update Page For Que Phuong

--- src/main/java/com/lims/controller/BooksController.java
+++ src/main/java/com/lims/controller/BooksController.java
@@ -8,34 +8,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lims.entity.Book;
 import com.lims.service.BookService;
 
 @Controller
 public class BooksController {
+	
 	@Autowired
 	BookService bookService;
 
 	@RequestMapping(value = "/book", method = RequestMethod.GET)
 	public String pageBook(Model model, Book book,
-			@RequestParam(value = "page", required = false, defaultValue = "0") int page
-			) {
+			@RequestParam(value = "page", required = false, defaultValue = "0") int page) {
+
+		page = page - 1;
 		model.addAttribute("books", bookService.getBookdAll());
-		Page<Book> pageBook = bookService.getBookAll(new PageRequest(page, 8));
-		model.addAttribute("bookPage", pageBook);
+		Page<Book> bookPage = bookService.getBookAll(new PageRequest(page, 8));
+		model.addAttribute("bookPage", bookPage);
 		return "views/books/book";
 	}
-//	@RequestMapping(value = "/book/list-book/page/{page}", method = RequestMethod.GET)
-//	
-//	public Page<Book> viewBookPage(Model model,
-//			@RequestParam(value = "page", required = false, defaultValue = "0") int page) {
-//		Page<Book> pageBook = bookService.getBookAll(new PageRequest(page, 8));
-//		model.addAttribute("bookPage", pageBook);
-//		System.out.println("tao ne hihi"+ pageBook.getNumber());
-//	
-//		return bookService.getBookAll(new PageRequest(page, 1));
-//	}
-	
 }
