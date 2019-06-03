0a1dc806447580a814d04a73db5a5b06b34f082a
sinhviencoder <48917649+sinhviencoder@users.noreply.github.com>
2019-06-03 16:28:40 +0700

    Initial For Que Phuong

--- src/main/java/com/lims/controller/admin/AdminMainController.java
+++ src/main/java/com/lims/controller/admin/AdminMainController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value = "/admin/")
+@RequestMapping(value = "/admin")
 public class AdminMainController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "", method = RequestMethod.GET)
 	public String index() {
 		return "admin/index";
 	}
