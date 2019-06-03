cec2a2ba7bf2b77e4123dc46cba884ac7f4fb1e1
sinhviencoder <48917649+sinhviencoder@users.noreply.github.com>
2019-06-03 21:02:20 +0700

    Update Login Page User And Multiple Login Page

--- src/main/java/com/lims/config/SecurityAdminConfig.java
+++ src/main/java/com/lims/config/SecurityAdminConfig.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,31 +17,42 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@Order(2)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Order(1)
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityAdminConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
-	
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/admin/**").csrf()
+		.disable().authorizeRequests()
+		.antMatchers("/admin/oss/**").permitAll()
+		.antMatchers("/admin/**").hasRole("ADMIN")
+				.antMatchers("/**").permitAll()
+				.anyRequest().authenticated().and().formLogin()
+				.loginPage("/admin/user/login").permitAll()
+				.usernameParameter("email").passwordParameter("password")
+				.defaultSuccessUrl("/admin/").and().logout().deleteCookies("remember-me").permitAll()
+				.and().rememberMe().tokenValiditySeconds(180);
+	}
+
 	@Bean
-	public PasswordEncoder encoder() {
+	public PasswordEncoder passwordEncoder() {
 		PasswordEncoder encoder = new BCryptPasswordEncoder();
 		return encoder;
 	}
 
+	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
 	@Override
-	  protected void configure(HttpSecurity http) throws Exception {
-		http.csrf()
-		.disable().authorizeRequests()
-				.anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll()
-				.usernameParameter("email").passwordParameter("password")
-				.defaultSuccessUrl("/").and().logout().deleteCookies("remember-me").permitAll()
-				.and().rememberMe().tokenValiditySeconds(180);
-	  }
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
 }
--- src/main/java/com/lims/controller/UserController.java
+++ src/main/java/com/lims/controller/UserController.java
@@ -1,20 +1,30 @@
 package com.lims.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.lims.entity.User;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
 
-	@RequestMapping(value = "/login_register", method = RequestMethod.GET)
-	public String registration(Model model) {
-		model.addAttribute("userForm", new User());
-
-		return "site.login_register";
+	@RequestMapping("/login")
+	public String login2(@RequestParam(required = false) String message, final Model model) {
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	      String name = auth.getName(); //get logged in username
+	      System.out.println(name);
+		if (message != null && !message.isEmpty()) {
+			if (message.equals("logout")) {
+				model.addAttribute("message", "Logout!");
+			}
+			if (message.equals("error")) {
+				model.addAttribute("message", "Login Failed!");
+			}
+		}
+		return "views/login-register";
 	}
 
 }
