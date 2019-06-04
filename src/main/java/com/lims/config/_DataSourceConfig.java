414d467744eb9eff6726bf9add13bb6f99b49cb8
Administrator <Administrator@MAY39>
2019-06-04 19:48:25 +0700

    Add Profiles dev and prod

--- src/main/java/com/lims/config/_DataSourceConfig.java
+++ src/main/java/com/lims/config/_DataSourceConfig.java
@@ -4,11 +4,13 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 @Configuration
+@Profile("dev")
 public class _DataSourceConfig {
 			
 		@Bean
--- src/main/java/com/lims/config/_SpringRootConfig.java
+++ src/main/java/com/lims/config/_SpringRootConfig.java
@@ -7,9 +7,11 @@ import org.hsqldb.util.DatabaseManagerSwing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
+@Profile("dev")
 public class _SpringRootConfig {
 
 	@Autowired
