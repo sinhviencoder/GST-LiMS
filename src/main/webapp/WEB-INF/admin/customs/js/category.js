a16fa675e881d459e3ffae6c043e62aed3be803f
Dell <Dell@DESKTOP-51B718R>
2019-06-16 20:34:35 +0700

    for edit comment

--- src/main/webapp/WEB-INF/admin/customs/js/category.js
+++ src/main/webapp/WEB-INF/admin/customs/js/category.js
@@ -34,14 +34,28 @@
                 "mRender": function (data, type, full) {
                      return '<button id="edit_post" style="margin-left: 5px" class="btn btn-sm btn-warning" value="'+ data.userId + '" data-toggle="tooltip" data-original-title="edit"><i class="fa fa-pencil-square-o"></i></button>'
                      +'<button id="detail_post" style="margin-left: 5px" class="btn btn-sm btn-info" value="'+ data.userId + '" data-toggle="tooltip" data-original-title="detail"><i class="fa fa-align-justify"></i></button>'
-                     +'<button id="delete_post" style="margin-left: 5px" class="btn btn-sm btn-danger" value="'+ data.userId + '" data-toggle="tooltip" data-original-title="delete"><i class="fa fa-trash-o"></i></button>';
+                     +'<button id="delete_post" style="margin-left: 5px" class="btn btn-sm btn-danger" value=" '+ data.userId + '" data-toggle="tooltip" data-original-title="delete"><i class="fa fa-trash-o"></i></button>';
                  }
              }
           ],
     });
 
-	$('button#delete_post').on('click', function(e){
-		deleteCategory(id);
+	$('#delete_post').on('click',function(){
+		let categoryID = $(this).attr('data');
+		console.log(categoryID);
+	
+		$.ajax({
+			type : "DELETE",
+			url : "/admin/category/delete/" +categoryID,
+			//data : $(id :'#categoryID'),
+			//global : false,
+			 success: function (result) {       
+		           console.log(result);                
+		    },
+		    error: function (e) {
+		        console.log(e);
+		    }
+	});
 	});
 	
 	$('#category-get-form').on('click', function(e) {
